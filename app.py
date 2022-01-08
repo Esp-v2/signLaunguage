19895dabf716e558e62371b8f9d841ab3151c3de
Esp-v2 <projectdaruma@gmail.com>
2022-01-09 02:15:29 +0900

    streamlit

--- app.py
+++ app.py
@@ -3,45 +3,164 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import cv2
 import av
-from PIL import Image
+#from PIL import Image
 import numpy as np
+#import pandas as pd
+import tensorflow as tf
 
-mp_face_mesh = mp.solutions.face_mesh
-#mp_face_detection = mp.solutions.face_detection
+# 参考
+# streamlit https://zenn.dev/whitphx/articles/streamlit-realtime-cv-app
+###
+
+
+# mediapipe初期設定
+mp_holistic = mp.solutions.holistic
+holistic = mp_holistic.Holistic(
+    static_image_mode=True,
+    min_detection_confidence=0.5)
 mp_drawing = mp.solutions.drawing_utils
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 
-st.title("リアルタイム手話認識")
+# 顔のランドマーク
+def face(results, annotated_image, label, csv):
+    if results.face_landmarks:
+        mp_drawing.draw_landmarks(
+            image=annotated_image,
+            landmark_list=results.face_landmarks,
+            connections=mp_holistic.FACEMESH_TESSELATION,
+            landmark_drawing_spec=drawing_spec,
+            connection_drawing_spec=drawing_spec)
 
-###
-#  参考 https://zenn.dev/whitphx/articles/streamlit-realtime-cv-app
-###
+        for index, landmark in enumerate(results.face_landmarks.landmark):
+            label.append("face_"+str(index) + "_x")
+            label.append("face_"+str(index) + "_y")
+            label.append("face_"+str(index) + "_z")
+            csv.append(landmark.x)
+            csv.append(landmark.y)
+            csv.append(landmark.z)
+    else:
+        for index in range(468):
+            label.append("face_"+str(index) + "_x")
+            label.append("face_"+str(index) + "_y")
+            label.append("face_"+str(index) + "_z")
+            for _ in range(3):
+                csv.append(np.nan)
+    return label, csv
+
+
+# 右手のランドマーク
+def r_hand(results, annotated_image, label, csv):
+    if results.right_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image=annotated_image,
+            landmark_list=results.right_hand_landmarks,
+            connections=mp_holistic.HAND_CONNECTIONS)
+
+        for index, landmark in enumerate(results.right_hand_landmarks.landmark):
+            label.append("r_hand_"+str(index) + "_x")
+            label.append("r_hand_"+str(index) + "_y")
+            label.append("r_hand_"+str(index) + "_z")
+            csv.append(landmark.x)
+            csv.append(landmark.y)
+            csv.append(landmark.z)
+    else:
+        for index in range(21):
+            label.append("r_hand_"+str(index) + "_x")
+            label.append("r_hand_"+str(index) + "_y")
+            label.append("r_hand_"+str(index) + "_z")
+            for _ in range(3):
+                csv.append(np.nan)
+    return label, csv
+
+
+# 左手のランドマーク
+def l_hand(results, annotated_image, label, csv):
+    if results.left_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image=annotated_image,
+            landmark_list=results.left_hand_landmarks,
+            connections=mp_holistic.HAND_CONNECTIONS)
+
+        for index, landmark in enumerate(results.left_hand_landmarks.landmark):
+            label.append("l_hand_"+str(index) + "_x")
+            label.append("l_hand_"+str(index) + "_y")
+            label.append("l_hand_"+str(index) + "_z")
+            csv.append(landmark.x)
+            csv.append(landmark.y)
+            csv.append(landmark.z)
+    else:
+        for index in range(21):
+            label.append("l_hand_"+str(index) + "_x")
+            label.append("l_hand_"+str(index) + "_y")
+            label.append("l_hand_"+str(index) + "_z")
+            for _ in range(3):
+                csv.append(np.nan)
+    return label, csv
+
+
+# 姿勢のランドマーク
+def pose(results, annotated_image, label, csv):
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(
+            image=annotated_image,
+            landmark_list=results.pose_landmarks,
+            connections=mp_holistic.HAND_CONNECTIONS)
+
+        for index, landmark in enumerate(results.pose_landmarks.landmark):
+            label.append("pose_"+str(index) + "_x")
+            label.append("pose_"+str(index) + "_y")
+            label.append("pose_"+str(index) + "_z")
+            csv.append(landmark.x)
+            csv.append(landmark.y)
+            csv.append(landmark.z)
+    else:
+        for index in range(33):
+            label.append("pose_"+str(index) + "_x")
+            label.append("pose_"+str(index) + "_y")
+            label.append("pose_"+str(index) + "_z")
+            for _ in range(3):
+                csv.append(np.nan)
+    return label, csv
+
+
+# mediapipeでランドマークを出力する
+def landmark(image):
+    results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    annotated_image = image.copy()
+
+    label = []
+    csv = []
+
+    #label, csv = face(results, annotated_image, csv, label)
+    label, csv = r_hand(results, annotated_image, label, csv)
+    #label, csv = l_hand(results, annotated_image, csv, label)
+    label, csv = pose(results, annotated_image, label, csv)
+
+    # 全フレームのランドマークを結合する
+    multi_landmarks.append(csv)
+
+    # multi_landmarksの末尾60フレームで識別する
+    if len(multi_landmarks) > 60:
+        array_landmark = np.array(multi_landmarks[-60:])
+        array_landmark = np.nan_to_num(array_landmark, nan=0.1)
+
+        pred = model.predict(array_landmark[None, ...])
+        print(pred.argmax())
+
+    return annotated_image
+
+
 class VideoProcessor:
-    def landmark(self,image):
-        with mp_face_mesh.FaceMesh(
-            static_image_mode=True,
-            max_num_faces=2,
-            min_detection_confidence=0.5) as face_mesh:
-            # Convert the BGR image to RGB and process it with MediaPipe Face Mesh.
-            results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-            # Draw face landmarks of each face.
-            annotated_image = image.copy()
-            for face_landmarks in results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
-                    image=annotated_image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=drawing_spec,
-                    connection_drawing_spec=drawing_spec)
-            return annotated_image
-
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
-        #img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)  # エッジ検出
-        img = self.landmark(img)  # mediapipeでのランドマーク検出
+        img = landmark(img)  # mediapipeでのランドマーク検出
+
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
+st.title("リアルタイム手話認識")
+
+multi_landmarks = [[]]
+model = tf.keras.models.load_model('LSTM.hdf5')
 webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
